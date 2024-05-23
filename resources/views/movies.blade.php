a517c59316537e4b210af8595021026b9700883d
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-23 18:08:31 +0200

    Inserita classe bootstrap text center.

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -11,11 +11,11 @@
     @foreach ($films as $film)
         <div class="card" style="width: 18rem; margin-bottom: 1rem;">
             <div class="card-body">
-                <p class="card-title"><strong>Titolo</strong>: {{ $film->title }}</p>
-                <p class="card-text"><strong>Titolo originale</strong>: {{ $film->original_title }}</p>
-                <p class="card-text"><strong>Paese</strong>: {{ $film->nationality }}</p>
-                <p class="card-text"><strong>Uscita</strong>: {{ $film->date }}</p>
-                <p class="card-text"><strong>Voto</strong>: {{ $film->vote }}</p>
+                <p class="card-title text-center"><strong>Titolo</strong>: {{ $film->title }}</p>
+                <p class="card-text text-center"><strong>Titolo originale</strong>: {{ $film->original_title }}</p>
+                <p class="card-text text-center"><strong>Paese</strong>: {{ $film->nationality }}</p>
+                <p class="card-text text-center"><strong>Uscita</strong>: {{ $film->date }}</p>
+                <p class="card-text text-center"><strong>Voto</strong>: {{ $film->vote }}</p>
             </div>
         </div>
     @endforeach
